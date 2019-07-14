686794ec583141b0313735f33c3636f6e9a60a9d
Timo Ikonen <timo.ikonen@iki.fi>
2019-07-14 22:26:44 +0300

    refactoring

--- magic_card_detector.py
+++ magic_card_detector.py
@@ -6,6 +6,7 @@ import glob
 import cProfile
 import pstats
 import io
+from itertools import product
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -114,25 +115,25 @@ def four_point_transform(image, poly):
     return warped
 
 
-def line_intersection(x_p, y_p):
+def line_intersection(x, y):
     """
     Calculates the intersection point of two lines, defined by the points
     (x1, y1) and (x2, y2) (first line), and
     (x3, y3) and (x4, y4) (second line).
     If the lines are parallel, (nan, nan) is returned.
     """
-    if (x_p[0] - x_p[1]) * (y_p[2] - y_p[3]) == (y_p[0] - y_p[1]) * (x_p[2] - x_p[3]):
+    if (x[0] - x[1]) * (y[2] - y[3]) == (y[0] - y[1]) * (x[2] - x[3]):
         # parallel lines
         xis = np.nan
         yis = np.nan
     else:
-        xis = ((x_p[0] * y_p[1] - y_p[0] * x_p[1]) * (x_p[2] - x_p[3]) -
-               (x_p[0] - x_p[1]) * (x_p[2] * y_p[3] - y_p[2] * x_p[3])) / \
-              ((x_p[0] - x_p[1]) * (y_p[2] - y_p[3]) - (y_p[0] - y_p[1]) * (x_p[2] - x_p[3]))
+        xis = ((x[0] * y[1] - y[0] * x[1]) * (x[2] - x[3]) -
+               (x[0] - x[1]) * (x[2] * y[3] - y[2] * x[3])) / \
+              ((x[0] - x[1]) * (y[2] - y[3]) - (y[0] - y[1]) * (x[2] - x[3]))
 
-        yis = ((x_p[0] * y_p[1] - y_p[0] * x_p[1]) * (y_p[2] - y_p[3]) -
-               (y_p[0] - y_p[1]) * (x_p[2] * y_p[3] - y_p[2] * x_p[3])) / \
-              ((x_p[0] - x_p[1]) * (y_p[2] - y_p[3]) - (y_p[0] - y_p[1]) * (x_p[2] - x_p[3]))
+        yis = ((x[0] * y[1] - y[0] * x[1]) * (y[2] - y[3]) -
+               (y[0] - y[1]) * (x[2] * y[3] - y[2] * x[3])) / \
+              ((x[0] - x[1]) * (y[2] - y[3]) - (y[0] - y[1]) * (x[2] - x[3]))
     return (xis, yis)
 
 
@@ -176,6 +177,7 @@ def simplify_polygon(in_poly, tol=0.05, maxiter=None, segment_to_remove=None):
 
     return out_poly
 
+
 def generate_point_indices(index_1, index_2, max_len):
     """
     Returns the four indices that give the end points of
@@ -186,6 +188,38 @@ def generate_point_indices(index_1, index_2, max_len):
                      (index_1 + 1) % max_len,
                      index_2 % max_len,
                      (index_2 + 1) % max_len])
+
+
+def generate_quad_corners(indices, x_s, y_s):
+    """
+    Returns the four intersection points from the
+    segments defined by the x coordinates (x_s),
+    y coordinates (y_s), and the indices.
+    """
+    (i, j, k, l) = indices
+
+    def gpi(index_1, index_2):
+        return generate_point_indices(index_1, index_2, len(x_s))
+
+    xis = np.empty(4)
+    yis = np.empty(4)
+    xis.fill(np.nan)
+    yis.fill(np.nan)
+
+    if j <= i or k <= j or l <= k:
+        pass
+    else:
+        (xis[0], yis[0]) = line_intersection(x_s[gpi(i, j)],
+                                             y_s[gpi(i, j)])
+        (xis[1], yis[1]) = line_intersection(x_s[gpi(j, k)],
+                                             y_s[gpi(j, k)])
+        (xis[2], yis[2]) = line_intersection(x_s[gpi(k, l)],
+                                             y_s[gpi(k, l)])
+        (xis[3], yis[3]) = line_intersection(x_s[gpi(l, i)],
+                                             y_s[gpi(l, i)])
+
+    return (xis, yis)
+
 
 def generate_quad_candidates(in_poly):
     """
@@ -203,58 +237,38 @@ def generate_quad_candidates(in_poly):
         np.asarray(in_poly.exterior.coords)[:-1, 1])
     quads = []
     len_poly = len(x_s)
-    for i in range(0, len_poly):
-        for j in range(i + 1, len_poly):
-            ind = generate_point_indices(i, j, len_poly)
-            (x_ij, y_ij) = line_intersection(x_s[ind], y_s[ind])
-            for k in range(j + 1, len_poly):
-                ind = generate_point_indices(j, k, len_poly)
-                (x_jk, y_jk) = line_intersection(x_s[ind], y_s[ind])
-                for l in range(k + 1, len_poly):
-                    ind = generate_point_indices(k, l, len_poly)
-                    (x_kl, y_kl) = line_intersection(x_s[ind], y_s[ind])
-                    ind = generate_point_indices(l, i, len_poly)
-                    (x_li, y_li) = line_intersection(x_s[ind], y_s[ind])
 
-                    xis = np.asarray([x_ij, x_jk, x_kl, x_li])
-                    yis = np.asarray([y_ij, y_jk, y_kl, y_li])
-
-                    if (np.sum(np.isnan(xis)) + np.sum(np.isnan(yis))) > 0:
-                        # no intersection point for some of the lines
-                        pass
-                    else:
-                        xis_ave = np.average(xis)
-                        yis_ave = np.average(yis)
-                        xis = xis_ave + 1.0001 * (xis - xis_ave)
-                        yis = yis_ave + 1.0001 * (yis - yis_ave)
-                        (xis, yis) = order_polygon_points(xis, yis)
-                        enclose = True
-                        quad = Polygon([(xis[0], yis[0]),
-                                        (xis[1], yis[1]),
-                                        (xis[2], yis[2]),
-                                        (xis[3], yis[3])])
-                        for x_i, y_i in zip(x_s, y_s):
-                            point = Point(x_i, y_i)
-                            if (not quad.intersects(point) and not quad.touches(point)):
-                                enclose = False
-                        if enclose:
-                            quads.append(quad)
+    for indices in product(range(len_poly), repeat=4):
+        (xis, yis) = generate_quad_corners(indices, x_s, y_s)
+        if (np.sum(np.isnan(xis)) + np.sum(np.isnan(yis))) > 0:
+            # no intersection point for some of the lines
+            pass
+        else:
+            xis_ave = np.average(xis)
+            yis_ave = np.average(yis)
+            xis = xis_ave + 1.0001 * (xis - xis_ave)
+            yis = yis_ave + 1.0001 * (yis - yis_ave)
+            (xis, yis) = order_polygon_points(xis, yis)
+            enclose = True
+            quad = Polygon([(xis[0], yis[0]),
+                            (xis[1], yis[1]),
+                            (xis[2], yis[2]),
+                            (xis[3], yis[3])])
+            for x_i, y_i in zip(x_s, y_s):
+                if (not quad.intersects(Point(x_i, y_i)) and
+                        not quad.touches(Point(x_i, y_i))):
+                    enclose = False
+            if enclose:
+                quads.append(quad)
     return quads
 
 
-def get_bounding_quad(hull, visual=False):
+def get_bounding_quad(hull_poly):
     """
     Returns the minimum area quadrilateral that contains (bounds)
     the convex hull (openCV format) given as input.
     """
-    hull_poly = Polygon([[x, y]
-                         for (x, y) in zip(hull[:, 0, 0], hull[:, 0, 1])])
     simple_poly = simplify_polygon(hull_poly)
-    if visual:
-        plt.plot(hull[:, 0, 0], hull[:, 0, 1], 's-')
-        plt.plot(simple_poly)
-        plt.title('hull and simplified')
-        plt.show()
     bounding_quads = generate_quad_candidates(simple_poly)
     bquad_areas = np.zeros(len(bounding_quads))
     for iquad, bquad in enumerate(bounding_quads):
@@ -268,28 +282,43 @@ def quad_corner_diff(hull_poly, bquad_poly, region_size=0.9):
     """
     Returns the difference between areas in the corners of a rounded
     corner and the aproximating sharp corner quadrilateral.
-    region_size (param) determines the region around the corner where the comparison is done.
+    region_size (param) determines the region around the corner where
+    the comparison is done.
     """
     bquad_corners = np.zeros((4, 2))
     bquad_corners[:, 0] = np.asarray(bquad_poly.exterior.coords)[:-1, 0]
     bquad_corners[:, 1] = np.asarray(bquad_poly.exterior.coords)[:-1, 1]
 
-    interior_x = np.average(
-        bquad_corners[:, 0]) + region_size * (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
-    interior_y = np.average(
-        bquad_corners[:, 1]) + region_size * (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
-    p0_x = interior_x + (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
-    p1_x = interior_x - (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
-    p0_y = interior_y - (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
-    p1_y = interior_y + (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
+    # The point inside the quadrilateral, region_size towards the quad center
+    interior_points = np.zeros((4, 2))
+    interior_points[:, 0] = np.average(bquad_corners[:, 0]) + \
+        region_size * (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
+    interior_points[:, 1] = np.average(bquad_corners[:, 1]) + \
+        region_size * (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
+
+    # The points p0 and p1 (at each corner) define the line whose intersections
+    # with the quad together with the corner point define the triangular
+    # area where the roundness of the convex hull in relation to the bounding
+    # quadrilateral is evaluated.
+    # The line (out of p0 and p1) is constructed such that it goes through the
+    # "interior_point" and is orthogonal to the line going from the corner to
+    # the center of the quad.
+    p0_x = interior_points[:, 0] + \
+        (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
+    p1_x = interior_points[:, 0] - \
+        (bquad_corners[:, 1] - np.average(bquad_corners[:, 1]))
+    p0_y = interior_points[:, 1] - \
+        (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
+    p1_y = interior_points[:, 1] + \
+        (bquad_corners[:, 0] - np.average(bquad_corners[:, 0]))
 
     corner_area_polys = []
-    for i in range(len(interior_x)):
+    for i in range(len(interior_points[:, 0])):
         bline = LineString([(p0_x[i], p0_y[i]), (p1_x[i], p1_y[i])])
-        corner_area_polys.append(Polygon([bquad_poly.intersection(bline).coords[0],
-                                          bquad_poly.intersection(
-                                              bline).coords[1],
-                                          (bquad_corners[i, 0], bquad_corners[i, 1])]))
+        corner_area_polys.append(Polygon(
+            [bquad_poly.intersection(bline).coords[0],
+             bquad_poly.intersection(bline).coords[1],
+             (bquad_corners[i, 0], bquad_corners[i, 1])]))
 
     hull_corner_area = 0
     quad_corner_area = 0
@@ -297,8 +326,32 @@ def quad_corner_diff(hull_poly, bquad_poly, region_size=0.9):
         quad_corner_area += capoly.area
         hull_corner_area += capoly.intersection(hull_poly).area
 
-    diff = 1. - hull_corner_area/quad_corner_area
-    return diff
+    return 1. - hull_corner_area/quad_corner_area
+
+
+def convex_hull_polygon(contour):
+    """
+    Returns the convex hull of the given contour as a polygon.
+    """
+    hull = cv2.convexHull(contour)
+    phull = Polygon([[x, y] for (x, y) in
+                     zip(hull[:, :, 0], hull[:, :, 1])])
+    return phull
+
+
+def polygon_form_factor(poly):
+    """
+    The ratio between the polygon area and circumference length,
+    scaled by the length of the shortest segment.
+    """
+    # minimum side length
+    d_0 = np.amin(np.sqrt(np.sum(np.diff(np.asarray(poly.exterior.coords),
+                                         axis=0) ** 2., axis=1)))
+    return poly.area/(poly.length * d_0)
+
+#
+# CLASSES
+#
 
 
 class CardCandidate:
@@ -350,16 +403,6 @@ class MTGCardDetector:
         self.clahe_clip_limit = 2.0
         self.clahe_tile_grid_size = (8, 8)
 
-    def polygon_form_factor(self, poly):
-        """
-        The ratio between the polygon area and circumference length,
-        scaled by the length of the shortest segment.
-        """
-        # minimum side length
-        d_0 = np.amin(np.sqrt(np.sum(np.diff(np.asarray(poly.exterior.coords),
-                                             axis=0) ** 2., axis=1)))
-        return poly.area/(poly.length * d_0)
-
     def read_and_adjust_images(self, path):
         """
         Reads image files and performs adaptive histogram equalization.
@@ -381,8 +424,8 @@ class MTGCardDetector:
             lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
             lightness, redness, yellowness = cv2.split(lab)
             corrected_lightness = clahe.apply(lightness)
-            limg = cv2.merge((corrected_lightness, redness, yellowness))
-            imgs_clahe.append(cv2.cvtColor(limg, cv2.COLOR_LAB2BGR))
+            img = cv2.merge((corrected_lightness, redness, yellowness))
+            imgs_clahe.append(cv2.cvtColor(img, cv2.COLOR_LAB2BGR))
 
         return (imgs_clahe, img_names)
 
@@ -392,18 +435,22 @@ class MTGCardDetector:
         Pre-calculates the hashes of the images.
         """
 
-        (self.ref_img_clahe, self.ref_filenames) = self.read_and_adjust_images(path)
+        (self.ref_img_clahe,
+         self.ref_filenames) = self.read_and_adjust_images(path)
         print('Hashing reference set.')
         for i in range(len(self.ref_img_clahe)):
-            self.phash_ref.append(imagehash.phash(PILImage.fromarray(np.uint8(
-                255 * cv2.cvtColor(self.ref_img_clahe[i], cv2.COLOR_BGR2RGB))), hash_size=32))
+            self.phash_ref.append(
+                imagehash.phash(
+                    PILImage.fromarray(np.uint8(255 * cv2.cvtColor(
+                        self.ref_img_clahe[i], cv2.COLOR_BGR2RGB))),
+                    hash_size=32))
 
     def read_and_adjust_test_images(self, path):
         """
         Reads and histogram-adjusts the reference image set.
         """
-
-        (self.test_img_clahe, self.test_filenames) = self.read_and_adjust_images(path)
+        (self.test_img_clahe,
+         self.test_filenames) = self.read_and_adjust_images(path)
 
     def segment_image(self, full_image):
         """
@@ -411,78 +458,44 @@ class MTGCardDetector:
         regions of the image that have a high chance
         of containing a recognizable card.
         """
-
         self.candidate_list.clear()
-
-        warped_list = []
-        card_list = []
-        bounding_poly_list = []
 
         image_area = full_image.shape[0] * full_image.shape[1]
         lca = 0.01  # largest card area
 
-        # grayscale transform, thresholding, countouring and contour sorting by area
+        # grayscale transform, thresholding, countouring and sorting by area
         gray = cv2.cvtColor(full_image, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, self.thr_lvl, 255, cv2.THRESH_BINARY)
         _, contours, _ = cv2.findContours(
             np.uint8(thresh), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours_s = sorted(contours, key=cv2.contourArea, reverse=True)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-        for card in contours_s:
-            hull = cv2.convexHull(card)
-            phull = Polygon([[x, y]
-                             for (x, y) in zip(hull[:, :, 0], hull[:, :, 1])])
+        for card_contour in contours:
+            phull = convex_hull_polygon(card_contour)
             if phull.area < 0.7 * lca:
                 # break after card size range has been explored
                 break
-            bounding_poly = get_bounding_quad(hull)
+            bounding_poly = get_bounding_quad(phull)
 
             qc_diff = quad_corner_diff(phull, bounding_poly)
-            if self.verbose:
-                print('Quad corner diff = ' + str(qc_diff))
+            scale_factor = min(1., (1. - qc_diff * 22. / 100.))
+            warped = four_point_transform(full_image,
+                                          scale(bounding_poly,
+                                                xfact=scale_factor,
+                                                yfact=scale_factor,
+                                                origin='centroid'))
 
-            if qc_diff > 0.05:
-                # typically indicates contour around the rounded card corners
-                crop_pct = qc_diff * 22.
-                if self.verbose:
-                    print('Cropping by ' + str(crop_pct) + ' %')
-            else:
-                crop_pct = 0.
-
-            scale_factor = (1.-crop_pct/100.)
-            bounding_poly_cropped = scale(bounding_poly, xfact=scale_factor,
-                                          yfact=scale_factor, origin='centroid')
-
-            warped = four_point_transform(full_image, bounding_poly_cropped)
-
-            form_factor = self.polygon_form_factor(bounding_poly)
-
-            if self.verbose:
-                print('Image area test (x < 0.99):')
-                print('x = ' + str(bounding_poly.area/image_area))
-                print('Shape test (0.27 < x 0.32):')
-                print('x = ' + str(form_factor))
-                print('Card area test (x > 0.7):')
-                print('x = ' + str(bounding_poly.area/lca))
-
-            if bounding_poly.area < image_area * 0.99:  # not the whole image test
-                if qc_diff < 0.35:  # corner shape test
-                    if form_factor > 0.27 and form_factor < 0.32:  # card shape test
-                        if bounding_poly.area > 0.7 * lca:  # card equal size test
-                            if lca == 0.01:
-                                lca = bounding_poly.area
-                            warped_list.append(warped)
-                            card_list.append(card)
-                            bounding_poly_list.append(bounding_poly)
-                            print(str(len(card_list)) + ' cards segmented.')
-
-        for im_seg, card_cnt, bquad in zip(warped_list,
-                                           card_list,
-                                           bounding_poly_list):
-            self.candidate_list.append(CardCandidate(im_seg,
-                                                     card_cnt,
-                                                     bquad,
-                                                     bquad.area/image_area))
+            if (0.7 * lca < bounding_poly.area < image_area * 0.99 and
+                    qc_diff < 0.35 and
+                    0.27 < polygon_form_factor(bounding_poly) < 0.32):
+                if lca == 0.01:
+                    lca = bounding_poly.area
+                self.candidate_list.append(
+                    CardCandidate(warped,
+                                  card_contour,
+                                  bounding_poly,
+                                  bounding_poly.area / image_area))
+                print(str(len(self.candidate_list)) + ' cards segmented.')
 
     def phash_compare(self, im_seg):
         """
@@ -613,7 +626,7 @@ def main():
     profiler.enable()
 
     # Run the card detection and recognition.
-    for im_ind in range(0, 7):
+    for im_ind in range(15, 30):
         card_detector.run_recognition(im_ind)
 
     # Stop profiling and organize and print profiling results.
