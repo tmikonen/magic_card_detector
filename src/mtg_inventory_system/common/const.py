e2d65cd463417560d50c513361989807c0f8be71
Matthew Buglass <matthewbuglass@gmail.com>
2022-11-05 16:42:56 -0600

    Working on building functions and relations for my models

--- src/mtg_inventory_system/common/const.py
+++ src/mtg_inventory_system/common/const.py
@@ -1,8 +1,33 @@
 from enum import Enum
 
-class CardColour(Enum):
-    RED = 'red'
-    GREEN = 'green'
-    BLUE = 'blue'
-    BLACK = 'black'
-    WHITE = 'white'
+
+PRINTING_TYPE_OPTIONS = [
+    ('NORMAL', 'normal'),
+    ('FOIL', 'foil'),
+    ('ETCHED', 'etched'),
+]
+
+
+CARD_LAYOUT_OPTIONS = [
+    ('NORMAL', 'normal'),
+    ('SPLIT', 'split'),
+    ('FLIP', 'flip'),
+    ('TRANSFORM', 'transform'),
+    ('MODAL_DFC', 'modal_dfc'),
+    ('MELD', 'meld'),
+    ('LEVELER', 'leveler'),
+    ('CLASS', 'class'),
+    ('SAGA', 'saga'),
+    ('ADVENTURE', 'adventure'),
+    ('PLANAR', 'planar'),
+    ('SCHEME', 'scheme'),
+    ('VANGUARD', 'vanguard'),
+    ('TOKEN', 'token'),
+    ('DOUBLE_FACED_TOKEN', 'double_faced_token'),
+    ('EMBLEM', 'emblem'),
+    ('AUGMENT', 'augment'),
+    ('HOST', 'host'),
+    ('ART_SERIES', 'art_series'),
+    ('REVERSIBLE_CARD', 'reversible_card'),
+]
+
--- src/mtg_inventory_system/common/models.py
+++ src/mtg_inventory_system/common/models.py
@@ -1,6 +1,9 @@
 import uuid
 
 from django.db import models
+from django.db.models import Count, F
+
+from const import CARD_LAYOUT_OPTIONS, PRINTING_TYPE_OPTIONS
 
 
 class User(models.Model):
@@ -8,48 +11,87 @@ class User(models.Model):
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
 
+    def get_card_library_query_set(self):
+        return Card.objects.filter(cardownership__user__id=self.id)
+
+    def get_unique_card_library_query_set(self):
+        return self.get_card_library_query_set().distinct()
+
+    def get_unique_card_library_count_query_set(self):
+        return self.get_card_library_query_set().annotate(count=Count('unique_string'))
+
 
 class Set(models.Model):
     name = models.CharField(max_length=500)
     scryfall_uuid = models.UUIDField()
 
 
+class ManaCost(models):
+    green = models.PositiveSmallIntegerField(null=True, default=None)
+    red = models.PositiveSmallIntegerField(null=True, default=None)
+    blue = models.PositiveSmallIntegerField(null=True, default=None)
+    black = models.PositiveSmallIntegerField(null=True, default=None)
+    white = models.PositiveSmallIntegerField(null=True, default=None)
+    colourless = models.PositiveSmallIntegerField(null=True, default=None)
+    x_mana = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('green', 'red', 'blue', 'black', 'white', 'colourless')
+
+
 class CardFace(models.Model):
     """Sometimes MTG cards can have more than one face tied to it. This represents that data
     """
-    name
-    mana
-    types
-    text
-    colours
-    small_img_uri
-    normal_img_uri
+    name = models.CharField(max_length=128)
+    mana_cost = models.ForeignKey(ManaCost, on_delete=models.DO_NOTHING)
+
+    power = models.SmallIntegerField(null=True)
+    toughness = models.SmallIntegerField(null=True)
+
+    type_line = models.CharField(max_length=100)
+    oracle_text = models.CharField(max_length=500)
+    small_img_uri = models.URLField()
+    normal_img_uri = models.URLField()
 
 
 class Card(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False)
-    scryfall_uri = models.CharField(max_length=500)
+    scryfall_uri = models.URLField()
+    scryfall_url = models.URLField()
 
-    is_two_sided = models.BooleanField(default=False)
-
-    # Primary Section
+    layout = models.CharField(max_length=20, choices=CARD_LAYOUT_OPTIONS)
     name = models.CharField(max_length=500)
-    power = models.IntegerField()
-    toughness = models.IntegerField()
 
-    small_img_url = models.CharField(max_length=500)
-    normal_img_url = models.CharField(max_length=500)
-
-    # Secondary Section
-
-    small_img_url_secondary = models.CharField(max_length=500, null=True)
-    normal_img_url_back = models.CharField(max_length=500, null=True)
+    conv_mana_cost = models.PositiveSmallIntegerField()
+    printing_type = models.CharField(max_length=10, choices=PRINTING_TYPE_OPTIONS)
 
     # Foreign Relations
+    face_primary = models.OneToOneField(CardFace, on_delete=models.CASCADE)
+    face_secondary = models.OneToOneField(CardFace, on_delete=models.CASCADE, null=True)
     set = models.ForeignKey(Set, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        unique_together = ('uuid', 'printing_type')
+
+    @property
+    def unique_string(self):
+        return "{} {}".format(self.uuid, self.printing_type)
+
 
 class CardOwnership(models.Model):
     # Foreign Relations
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     card = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
+    date_added = models.DateField(auto_now_add=True)
+    date_removed = models.DateField(null=True)
+    price_purchased = models.DecimalField(decimal_places=2)
+    price_sold = models.DecimalField(decimal_places=2, null=True)
+
+
+class CardPrice(models.Model):
+    """Tracks how much a card cost in USD on a certain day
+    """
+    date = models.DateField(auto_now_add=True)
+    price_usd = models.DecimalField(decimal_places=2)
+    card = models.OneToOneField(Card, on_delete=models.DO_NOTHING)
 
