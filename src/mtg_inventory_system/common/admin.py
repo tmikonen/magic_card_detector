70b26e7fa4fe4c5e860b828d53c8d5d27b810e34
Matthew Buglass <matthewbuglass@gmail.com>
2022-11-07 16:59:16 -0600

    Listed the common models to be viewed in the admin panel

--- src/mtg_inventory_system/common/admin.py
+++ src/mtg_inventory_system/common/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.register(User)
+admin.register(StorageLocation)
+admin.register(CardSet)
+admin.register(ManaCost)
+admin.register(CardFace)
+admin.register(Card)
+admin.register(CardOwnership)
+admin.register(CardPrice)
+admin.register(Deck)
+admin.register(ConstructedDeck)
+admin.register(CommanderDeck)
--- src/mtg_inventory_system/mtg_inventory_system/settings.py
+++ src/mtg_inventory_system/mtg_inventory_system/settings.py
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
