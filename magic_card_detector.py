7c0fbf010347139a11f55e60b21fe9d705633f3e
Timo Ikonen <timo.ikonen@iki.fi>
2025-12-21 08:01:14 +0200

    Cleanup, no functional changes

--- magic_card_detector.py
+++ magic_card_detector.py
@@ -9,7 +9,6 @@ import os
 import cProfile
 import pstats
 import io
-import base64
 import pickle
 import argparse
 from copy import deepcopy
@@ -387,15 +386,6 @@ class CardCandidate:
     recognition_score: float = 0.
     is_fragment: bool = False
     name: str = 'unknown'
-
-    # def __init__(self, im_seg, bquad, fraction):
-    #    self.image = im_seg
-    #    self.bounding_quad = bquad
-    #    self.is_recognized = False
-    #    self.recognition_score = 0.
-    #    self.is_fragment = False
-    #    self.image_area_fraction = fraction
-    #    self.name = 'unknown'
 
     def contains(self, other):
         """
